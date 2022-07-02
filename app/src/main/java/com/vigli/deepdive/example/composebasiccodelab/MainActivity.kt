b7445e1f3e7cdb19677006c7d3f1ee96c5ffd4af
vigli-Lee <wnsqjadl@gmail.com>
2022-07-02 22:44:49 +0900

    Creating a performant lazy list

--- app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -33,7 +35,7 @@ private fun MyApp() {
             shouldShowOnboarding = false
         }
     } else {
-        Greeting()
+        Greetings()
     }
 }
 
@@ -70,9 +72,9 @@ fun OnboardingPreview() {
 }
 
 @Composable
-private fun Greeting(names: List<String> = listOf("World", "Compose")) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
