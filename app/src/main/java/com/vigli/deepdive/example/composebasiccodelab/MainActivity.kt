7fbb2505a8432476366a82b46be049ac4879359c
vigli-Lee <wnsqjadl@gmail.com>
2022-07-02 23:10:28 +0900

    optimise import

--- app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.vigli.deepdive.example.composebasiccodelab
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,13 +10,14 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
