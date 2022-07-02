1ca1583b1ae5287ade049797b69f555c51c6deb6
vigli-Lee <wnsqjadl@gmail.com>
2022-07-02 23:06:27 +0900

    Styling and theming my app

--- app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.vigli.deepdive.example.composebasiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +17,8 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vigli.deepdive.example.composebasiccodelab.ui.theme.ComposeBasicCodeLabTheme
@@ -112,7 +116,12 @@ private fun Greeting(name: String) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.h4.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             OutlinedButton(
                 onClick = { expanded.value = expanded.value.not() }
@@ -127,11 +136,13 @@ private fun Greeting(name: String) {
 @Preview(
     showBackground = true,
     widthDp = 320,
+//    uiMode = UI_MODE_NIGHT_NO,
+    uiMode = UI_MODE_NIGHT_YES,
     name = "Text Preview"
 )
 @Composable
 fun DefaultPreview() {
     ComposeBasicCodeLabTheme {
-        MyApp()
+        Greetings()
     }
 }
