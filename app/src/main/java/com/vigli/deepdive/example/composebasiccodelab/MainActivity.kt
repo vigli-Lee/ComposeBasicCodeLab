6e2681279833cd2e06cb265d60813fe84189ae6e
vigli-Lee <wnsqjadl@gmail.com>
2022-07-02 22:49:11 +0900

    Persisting state

--- app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/com/vigli/deepdive/example/composebasiccodelab/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +30,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen {
@@ -83,7 +85,7 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 private fun Greeting(name: String) {
     // 상태 유지로 recomposition 방지
-    val expanded = remember {
+    val expanded = rememberSaveable {
         // 상태 변경 추적
         mutableStateOf(false)
     }
